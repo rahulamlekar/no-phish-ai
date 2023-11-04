b1b1c8387d13442c74bd4ff655ff5c8fcb178457
rahulamlekar <rahul.amlekar@mail.mcgill.ca>
2023-11-04 15:57:24 -0700

    security hello world

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.schema import SystemMessage
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
 os.environ['LANGCHAIN_API_KEY'] = 'redacted'
 os.environ['LANGCHAIN_PROJECT'] = 'no-phish-ai'
 os.environ['OPENAI_API_KEY'] = 'sk-redacted'
 
-
-from langchain.chat_models import ChatOpenAI
+chat_template = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content=(
+                "You are a security assistant that tells users about phishy sites."
+            )
+        ),
+        HumanMessagePromptTemplate.from_template("{text}"),
+    ]
+)
 
 llm = ChatOpenAI()
-llm.invoke("Hello, world!")
+llm(chat_template.format_messages(text="google.com"))
