5674991eaeadb0c12b381a438c7832265062ec11
rahulamlekar <rahul.amlekar@mail.mcgill.ca>
2023-11-04 19:30:49 -0700

    organizing directory

--- archive/main.py
+++ archive/main.py
@@ -4,9 +4,9 @@ from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.schema import SystemMessage
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
-os.environ['LANGCHAIN_API_KEY'] = 'redacted'
+os.environ['LANGCHAIN_API_KEY'] = 'ls__85291a99f2ab490bab7da20a3f9b254e'
 os.environ['LANGCHAIN_PROJECT'] = 'no-phish-ai'
-os.environ['OPENAI_API_KEY'] = 'sk-redacted'
+os.environ['OPENAI_API_KEY'] = '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'
 
 chat_template = ChatPromptTemplate.from_messages(
     [
